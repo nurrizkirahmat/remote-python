8ae02861683e25eb85923725ec32b2a69737952c
nurrizkirahmat <rizkyr.nur@gmail.com>
2020-06-30 20:57:38 +0700

    Initial Test Commit

---
+++ casting.py
@@ -0,0 +1,11 @@
+x = 10
+y = 5
+print(x + y)
+print(str(x) + str(y))
+
+
+
+x = int(input('masukan angka pertama: '))
+y = int(input('masukan angka ke dua: '))
+result = x + y
+print(result)
---
+++ try.py
@@ -0,0 +1,7 @@
+try:
+	x = 10
+	y = 0
+	res = x//y
+	print(res)
+except ZeroDivisionError:
+	print("Error Occured")
